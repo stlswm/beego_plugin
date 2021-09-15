4cbfdc7ffc96b073ae11652773eb99866116fb91
wangmin <691207572@qq.com>
2021-09-15 16:57:08 +0800

    升级beego2

--- encrypt/aes128/func.go
+++ encrypt/aes128/func.go
@@ -1,20 +1,20 @@
 package aes128
 
 import (
-	"github.com/astaxie/beego"
+	"github.com/beego/beego/v2/server/web"
 	"github.com/stlswm/goencrypt/aes128"
 )
 
 // 加密
 func Encrypt(src string) string {
-	key := beego.AppConfig.String("encrypt.key")
-	iv := beego.AppConfig.String("encrypt.iv")
+	key, _ := web.AppConfig.String("encrypt.key")
+	iv, _ := web.AppConfig.String("encrypt.iv")
 	return aes128.Encrypt(src, key, iv)
 }
 
 // 解密
 func Decrypt(src string) string {
-	key := beego.AppConfig.String("encrypt.key")
-	iv := beego.AppConfig.String("encrypt.iv")
+	key, _ := web.AppConfig.String("encrypt.key")
+	iv, _ := web.AppConfig.String("encrypt.iv")
 	return aes128.Decrypt(src, key, iv)
 }
